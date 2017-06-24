9541d339c349dcead983d059a4997119a1d7be47
shallav <dhruva.kumar08@gmail.com>
2017-06-24 18:47:02 -0400

    adding generator and visualizing loss

--- model.py
+++ model.py
@@ -3,41 +3,65 @@ import cv2
 import pickle
 import numpy as np
 import time
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import sklearn
+import pickle
+import matplotlib.pyplot as plt
 
 
 # parse csv file to read in images and steering angles
 # csv format: center,left,right,steering,throttle,brake,speed
 
 print('Loading data...')
-start = time.time()
-images = []
-measurements = []
+
+# read in image paths from csv file
+samples = []
 with open('data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
-		img_filename = 'data/' + line[0] # taking only center images for now
-		image = cv2.imread(img_filename)
-		images.append(image)
-		measurements.append(float(line[3]))
+		samples.append(line)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
-print(y_train.shape, ' images loaded in ', time.time() - start, 's')
+# generate training and validation data
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-# pickle.dump({"X_train": images, "y_train": measurements}, open('data.p', 'wb'))
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1:
+		samples = shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
 
-# load data
-# start = time.time()
-# data = pickle.load(open('data.p', 'rb'))
+			images = []
+			angles = []
+			for batch_sample in batch_samples:
+				# TODO: load in other images too
+				# TODO: change from BGR to RGB
+				center_img_filename = 'data/' + batch_sample[0]
+				center_image = cv2.imread(center_img_filename)
+				images.append(center_image)
+				angles.append(float(batch_sample[3]))
+		
+			X = np.array(images)
+			y = np.array(angles)	
+			yield shuffle(X, y)
 
-# X_train = np.array(data["X_train"])
-# y_train = np.array(data["y_train"])
-# print(y_train.shape, ' images loaded in ', time.time() - start, 's')
+batch_size = 32
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
 
 
-from keras.models import Sequential
+print('Training...')
+print('training data=',len(train_samples))
+print('validation data=',len(validation_samples))
+print('batch_size=',batch_size)
+
+from keras.models import Sequential, Model
 from keras.layers.core import Dense, Flatten, Lambda, Activation
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
+start = time.time()
 
 # build and train a regression model (LeNet)
 model = Sequential()
@@ -53,9 +77,25 @@ model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')											
-model.fit(X_train, y_train, nb_epoch=6, validation_split=0.2, shuffle=True)
+model_history = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), 
+	validation_data=validation_generator, nb_val_samples=len(validation_samples) nb_epoch=6, verbose=1)
 model.save('model.h5')
 
+print('training completed in ', time.time() - start, 's')
+
+
+# save model history to file
+pickle.dump(model_history.history, open("model_loss.p", "wb"))
+
+# plot the training and validation loss for each epoch
+plt.plot(model_history.history['loss'])
+plt.plot(model_history.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('loss_mse.png')
+plt.show()
 
 
 
