6f2b0da165da99cf4850c44177addd9ffc467b5c
DhruvaKumar <dhruva.kumar08@gmail.com>
2017-06-25 19:35:49 +0000

    reduced epochs and switched plot to backend

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from sklearn.utils import shuffle
 import sklearn
 import pickle
 import matplotlib.pyplot as plt
-
+plt.switch_backend('agg')
 
 # parse csv file to read in images and steering angles
 # csv format: center,left,right,steering,throttle,brake,speed
@@ -107,7 +107,7 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')											
 model_history = model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*2, 
-	validation_data=validation_generator, nb_val_samples=len(validation_samples)*2, nb_epoch=7, verbose=1)
+	validation_data=validation_generator, nb_val_samples=len(validation_samples)*2, nb_epoch=4, verbose=1)
 model.save('model.h5')
 
 print('training completed in ', time.time() - start, 's')
